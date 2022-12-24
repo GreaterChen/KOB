712b5723a0de2e14c2a0c9d2c7920d7c4d39a670
clb <1796390642@qq.com>
2022-12-25 00:08:37 +0800

    骨架搭建

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,11 +3,16 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("getbotinfo/")
-    public String getBotInfo(){
-        return "hhhh";
+    public HashMap<String, String> getBotInfo(){
+        HashMap<String,String> m= new HashMap<>();
+        m.put("name","tiger");
+        m.put("rating","1500");
+        return m;
     }
 }
