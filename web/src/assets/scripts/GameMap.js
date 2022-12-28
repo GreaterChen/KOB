8e0c9248c35ce12d41e5a40773aa1b76cfd16260
clb <1796390642@qq.com>
2022-12-28 13:06:01 +0800

    “该后端了吧”

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,4 +1,5 @@
 import { AcGameObject } from "./AcGameObject";
+import { Snake } from "./Snake";
 import { Wall } from "./wall";
 export class GameMap extends AcGameObject{
     constructor(ctx,parent){
@@ -7,12 +8,19 @@ export class GameMap extends AcGameObject{
         this.parent = parent;
         this.L = 0;
         this.rows = 13;
-        this.cols = 13;
+        this.cols = 14;
 
         this.walls = [];
         this.inner_walls_count = 20;
 
+        this.snakes = [
+            new Snake({id:0,color:"#4876EC",r:this.rows-2,c:1},this),
+            new Snake({id:0,color:"#F94848",r:1,c:this.cols-2},this),
+        ]
+
     }
+
+
 
     check_connectivity(g,sx,sy,tx,ty){
         if(sx==tx && sy==ty) return true;
@@ -48,10 +56,10 @@ export class GameMap extends AcGameObject{
             for(let j=0;j<1000;j++){
                 let r = parseInt(Math.random()*this.rows);
                 let c = parseInt(Math.random()*this.cols);
-                if(g[r][c] || g[c][r]) continue;
+                if(g[r][c] || g[this.rows-1-r][this.cols-1-c]) continue;
                 if(r==this.rows-2&&c==1 || r==1&c==this.cols-2) continue;
 
-                g[r][c] = g[c][r] = true;
+                g[r][c] = g[this.rows-1-r][this.cols-1-c] = true;
                 break;
             }
         }
@@ -72,10 +80,42 @@ export class GameMap extends AcGameObject{
         return true;
     }
 
+    add_listening_events(){
+        this.ctx.canvas.focus();
+
+        const [snake0,snake1] = this.snakes;
+        this.ctx.canvas.addEventListener("keydown",e=>{
+            if(e.key === 'w') snake0.set_direction(0);
+            else if(e.key === 'd') snake0.set_direction(1);
+            else if(e.key === 's') snake0.set_direction(2);
+            else if(e.key === 'a') snake0.set_direction(3);
+            else{
+                switch(e.key){
+                    case 'ArrowUp': 
+                        snake1.set_direction(0);
+                        break;
+                    case 'ArrowDown':
+                        snake1.set_direction(2);
+                        break;
+                    case 'ArrowRight':
+                        snake1.set_direction(1);
+                        break;
+                    case 'ArrowLeft':
+                        snake1.set_direction(3);
+                        break;
+                    default:
+                        console.log("无效输入");
+            }
+            }
+        })
+    }
+
+
     start(){
         for(let i=0;i<1000;i++){
             if(this.create_walls()) break;
         }
+        this.add_listening_events();
     }
 
     update_size(){
@@ -83,8 +123,44 @@ export class GameMap extends AcGameObject{
         this.ctx.canvas.width = this.L*this.cols;
         this.ctx.canvas.height = this.L*this.rows; 
     }
+
+    check_ready(){
+        for(const snake of this.snakes){
+            if(snake.status !== "idle") return false;
+            if(snake.direction === -1) return false; 
+        }
+        return true;
+    }
+
+    check_valid(cell) {  // 检测目标位置是否合法：没有撞到两条蛇的身体和障碍物
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c)
+                return false;
+        }
+
+        for (const snake of this.snakes) {
+            let k = snake.cells.length;
+            if (!snake.check_tail_increasing()) {  // 当蛇尾会前进的时候，蛇尾不要判断
+                k -- ;
+            }
+            for (let i = 0; i < k; i ++ ) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
     update(){
         this.update_size();
+        
+        if(this.check_ready()){
+            for(const snake of this.snakes){
+                snake.next_step();
+            }
+        }
+
         this.render();
     }
     
